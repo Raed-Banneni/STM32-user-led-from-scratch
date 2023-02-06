162b682cfd95d94e4046449c0bbea119d5f92c2b
Raed Banneni <raed.banneni@etudiant-fst.utm.tn>
2023-02-07 00:37:47 +0100

    turning user led with structures & OD register

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# STM32-user-led-from-scratch
+# STM32-user-led-from-scratch 
+ in this commit I tried to work with strucre type definitions to facilitate the code a little bit and understand how structures work 
+ PS: this is not mendatory you can simply work with the header files provided by CMSIS i was just trying to understand how thing operate
--- Src/main.c
+++ Src/main.c
@@ -1,52 +1,79 @@
-//WHERE THE LED IS CONNECTED
-//PORT: A
-//PIN: 5
-#define PERIPH_BASE 		(0x40000000UL)
+//we must locate port and pin number
+// Port A
+// pin  5
+#include <stdint.h>
+#define periph_base 			(0x40000000UL)
 
-#define AHB2_PERIPH_OFFSET	(0x08000000UL)
-#define AHB2_PERIPH_BASE 	(PERIPH_BASE + AHB2_PERIPH_OFFSET)
+#define ahb2_offset				(0x08000000UL)
+#define ahb2_base  				(periph_base + ahb2_offset)
 
-#define GPIOA_OFFSET		(0x0000UL) // OFFSETfrom AHB2bus
-#define GPIOA_BASE 			(AHB2_PERIPH_BASE + GPIOA_OFFSET)
+#define ahb1_offset				(0x20000UL)
+#define ahb1_base				(periph_base + ahb1_offset)
 
-#define AHB1_PERIPH_OFFSET	(0x00020000UL)
-#define AHB1_PERIPH_BASE	(PERIPH_BASE + AHB1_PERIPH_OFFSET)
+#define gpioa_offset 			(0x000000UL)
+#define gpioa_base				(ahb2_base + gpioa_offset)
 
-#define RCC_OFFSET			(0x1000UL) // OFFSETfrom AHB2 bus
-#define RCC_BASE 			(AHB1_PERIPH_BASE + RCC_OFFSET)
+#define rcc_offset				(0X1000UL)
+#define rcc_base				(ahb1_base + rcc_offset)
+
+#define rcc_ahb2enr_offset 		(0x4CUL)
+#define rcc_ahb2enr				(*(volatile unsigned int *)(rcc_base + rcc_ahb2enr_offset))
+
+#define gpioa_moder_offset		(0x00UL)
+#define gpioa_moder 			(*(volatile unsigned int*)(gpioa_base +gpioa_moder_offset))
+
+#define odr_offset 				(0X14UL)
+#define odr 					(*(volatile unsigned int*)(gpioa_base + odr_offset))
+
+#define gpioa_en				(1U<<0)
+
+#define pin5					(1U<<5)
+#define led_pin					pin5
 
 
-#define AHB2EN_R_OFFSET		(0x4CUL)	//REGISTER OFFSET
-#define RCC_AHB2EN_R			(*(volatile unsigned int *)( RCC_BASE + AHB2EN_R_OFFSET)) //THE REGISTER
+typedef struct
+{	//REGISTERS SHOULD BE IN THE SAME ORDER AS IN THE RM EACH ONE OCCUPIES 4 BYTES
+	volatile uint32_t MODER;
+//	volatile uint32_t OTYPER;
+//	volatile uint32_t OSPEEDR;
+//	volatile uint32_t PUPDR;
+//	volatile uint32_t IDR;
+	volatile uint32_t DUMMY[4]; //we added this dummy array to keep the same offset between the only two registers that we need.
+	volatile uint32_t ODR;
+//	volatile uint32_t BSRR;
+//	volatile uint32_t LCKR;
+//	volatile uint32_t AFR[2];
+//	volatile uint32_t PBRR;
+//	volatile uint32_t PASCR;
+//
+}gpio_Typedef;
 
-#define MODE_R_OFFSET			(0x00)
-#define GPIOA_MODE_R			(*(volatile unsigned int *)(GPIOA_BASE + MODE_R_OFFSET))
-
-#define OD_R_OFFSET			(0x14)
-#define GPIO_OD_R 				(*(volatile unsigned int *)(GPIOA_BASE +OD_R_OFFSET))
-
-
-#define GPIOAEN					(1U<<0)	//SHIFT 1 POSITION 0
-										// 0B 0000 0000 0000 0000 0000 0000 0000 0001
-#define PIN5				    (1U<<5)
-#define LED_PIN					PIN5
-
-/*MODE REGISTER BITS 10 AND 11 ARE PIN 5 MODE BITS
- * FOR OUTPUT IT MUST BE 01
- * (1U<<10) // sets only bit 10 to 1
- * &=~(1U<<11)0 sets only bit 11 to 0
- */
-
-int main (void)
+typedef struct
 {
-	//1. enable clock access to GPIOA
-	RCC_AHB2EN_R |= GPIOAEN;
-	//2.SET pA5 to output
-	 GPIOA_MODE_R |= (1U<<10);
-	 GPIOA_MODE_R &=~(1U<<11);
+	volatile uint32_t DUMMY[19];
+	volatile uint32_t AHB2ENR;
+}rcc_Typedef;
+
+
+#define RCC 		((rcc_Typedef*) rcc_base)
+#define GPIOA 		((gpio_Typedef*) gpioa_base)
+
+int main(void)
+{
+	//	rcc_ahb2enr |= gpioa_en;
+	RCC->AHB2ENR    |= gpioa_en;
+
+//	gpioa_moder 	|= (1U<<10);
+//	gpioa_moder 	&=~(1U<<11);
+
+	GPIOA->MODER 	|= (1U<<10);
+	GPIOA->MODER 	&=~ (1U<<11);
+
 	while(1)
 	{
-		// 3 . set pa5 high
-		GPIO_OD_R |=LED_PIN ;
+		//odr |=led_pin;
+		GPIOA->ODR  ^= led_pin;
+		for(volatile long int i =0; i<100000;i++){}
 	}
 }
+
