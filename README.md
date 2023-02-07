b3251f06a6712654a21ef03ab152951d387288e4
Raed Banneni <raed.banneni@etudiant-fst.utm.tn>
2023-02-07 01:03:16 +0100

    used IDR for button

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
  PS: this is not mendatory you can simply work with the header files provided by CMSIS i was just trying to understand how thing operate
 ************************************
 New commit : replaced all the definitions with the "stm32l-476xx" header include line.
+***********************************
+New commit : controlled the led with the user button 
--- Src/main.c
+++ Src/main.c
@@ -1,20 +1,33 @@
 #include "stm32l476xx.h"
 
+#define GPIOCEN	(1U<<2)
+#define PIN13 	(1U<<13)
+#define BTN     PIN13
+#define GPIOAEN (1U<<0)
+#define PIN5 	(1U<<5)
+#define LED_PIN PIN5
 
-#define GPIOAEN		(1U<<0)
-#define PIN5		(1U<<5)
-#define LED_PIN		PIN5
 
 
-int main(void)
+int main (void )
 {
-	RCC->AHB2ENR |=GPIOAEN;
+	RCC->AHB2ENR |= GPIOAEN;
+	RCC->AHB2ENR |= GPIOCEN;
+
 	GPIOA->MODER |=(1U<<10);
 	GPIOA->MODER &=~(1U<<11);
-	while(1)
+	/* SET PC13 AS INPUT */
+	GPIOC->MODER &=~(1U<<26);
+	GPIOC->MODER &=~(1U<<27);
+
+	while (1)
 	{
-		GPIOA->ODR ^=LED_PIN;
-		for (volatile  int i=0;i<1000000;i++){}
+		if (GPIOC->IDR && PIN13)
+
+		GPIOA->ODR &=~LED_PIN;
+
+		else
+		GPIOC->BSRR |=(1U<<21);
+
 	}
-	return 0;
 }
